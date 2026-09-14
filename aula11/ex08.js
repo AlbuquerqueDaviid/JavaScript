e48304cfe1d674670d41fbcb2978c514a06a3adb
AlbuquerqueDavid <132590326+AlbuquerqueDaviid@users.noreply.github.com>
2026-09-14 09:49:13 -0300

    Aula: Condições

--- aula11/ex08.js
+++ aula11/ex08.js
@@ -1,6 +1,8 @@
-var vel = 65
+var vel = 50
 console.log(`a velocidade do seu carro é ${vel} km/h`)
 if (vel > 60){ //Condição simples
     console.log(`você ultrapassou a velocidade permitida. MULTADO!`)
+} else {
+    console.log(`Continue em frente!`)
 }
 console.log(`Dirija sempre usando cinto de segurança!`)
